2b6065aab2713d09403a24564c056ec2a6949875
sergioescobarh <sergio.escobar.henao@gmail.com>
2022-09-17 13:20:50 -0500

    last change

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express= require('express');
 const app = express();
 const path= require('path');
 //Middleware
-//const publicPath = path.resolve(__dirname, '../Public');
+
 app.use(express.static('public'));
-app.set('puerto',process.env.PORT||3001);
+//app.set('puerto',process.env.PORT||3001);
 //rUTAS:
 
 app.get("/", (req, res) => {
@@ -19,9 +19,9 @@ app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "Vews/login.html"));
 });
 
+app.listen(process.env.PORT||3000,function(){console.log('listening on port 3000');});
 
-
-app.listen(ap.get('puerto'),() => console.log('servidor corriendo de manera satisfactoria'));
+//app.listen(ap.get('puerto'),() => console.log('servidor corriendo de manera satisfactoria'));
 
 
 
